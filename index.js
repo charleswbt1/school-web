eea3c8aab4641808c24d2182431e51ef220ad02c
charles <charles.wbt1@gmail.com>
2026-06-07 17:05:07 -0600

    fix index

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json())
 app.use(express.static(path.join(__dirname, 'src')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src', 'home/index.html'));
+  res.redirect('/home/index.html');
 });
 
 
